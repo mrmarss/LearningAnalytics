7a7ea2a6d76e5ad4f5c0f09e8e9a1e376a9258fe
nmaresco <nmarsco@bu.edu>
2016-01-28 12:14:11 -0500

    changed file structure to help simplicity

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var port  = 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// viewed at http://localhost:3000
+app.get('/', function(req, res) {
+    /* Send over the index html file */
+    res.sendFile(path.join(__dirname + '/index.html'));
+
+    /* Grab data beyond the basic directory */
+    app.use(express.static(__dirname + '/'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(port);
+
+console.log('server started on port %s', port);
